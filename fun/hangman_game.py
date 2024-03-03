68ce54db4a15f2dcd3d7d4c6d711921dfebbf824
zeitjo <jzeitler1990@gmail.com>
2024-03-02 22:15:28 -0500

    Updates 2024-03-02 :>>

--- fun/hangman_game.py
+++ fun/hangman_game.py
@@ -1,12 +1,18 @@
 from hangman_constants import HANGMANPICS, HANGMANWORDS
 
+mydict = {
+    "a": "aaaaaa",
+    "b": "bbbuuuuh",
+    "c": "c",
+}
 
 def hangman_game():
-    for pic in HANGMANPICS:
-        print(pic)
-
-    for word in HANGMANWORDS:
-        print(word)
+    is_running = True
+    while is_running:
+        print("Welcome to my hang, man")
+        input_option = input("Do you want to quit? (y/n) ")
+        if input_option.lower().strip() in {"y", "yes", "yeah", "yee"}:
+            is_running = False
 
 
 
