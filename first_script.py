13ba45d5ab11b8afc2f78181bbc198743d3708c2
zeitjo <jzeitler1990@gmail.com>
2024-04-13 21:19:50 -0400

    Delete disk_file.txt

--- first_script.py
+++ first_script.py
@@ -1,7 +1,9 @@
 def main():
     input_name = input("Enter name: ")
     print("Hello World!")
+    # This comment goes soooooo crazy
     print(f"Hello {input_name}!")
+    # I like to write so many comments
 
 
 if __name__ == "__main__":
